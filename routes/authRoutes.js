df5425f23c1392a1a79b4d401a8852cd878314a6
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2021-06-06 15:57:22 -0400

    logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,6 +12,12 @@ module.exports = (app) => {
   // use code
   app.get("/auth/google/callback", passport.authenticate("google"));
 
+  app.get("/api/logout", (req, res) => {
+    // removes cookie
+    req.logout();
+    res.send(req.user);
+  });
+
   app.get("/api/current_user", (req, res) => {
     res.send(req.user);
   });
