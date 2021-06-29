19478112e57b201676823954b6de29236a184338
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2021-06-29 17:53:45 -0400

    install root modules

---
+++ client/src/components/App.js
@@ -0,0 +1,8 @@
+// rendering layer control (React)
+import React from "react";
+
+const App = () => {
+  return <div>Hi There!</div>;
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,8 @@
 // data layer control (Redux)
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
