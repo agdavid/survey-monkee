53de4bfa20ae520df1558c3d365e08e9ffae1b8c
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2021-06-29 11:10:23 -0400

    fix typo

--- services/passport.js
+++ services/passport.js
@@ -23,7 +23,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
-      prox: true,
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((existingUser) => {
