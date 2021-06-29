a242845a7f58816a03fb0017be3947bd1f41df03
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2021-06-29 10:32:20 -0400

    environment keys setup

---
+++ config/schema.dev.js
@@ -0,0 +1,7 @@
+// dev.js - do not commit this
+module.exports = {
+  googleClientID: "myGoogleclientID",
+  googleClientSecret: "myGoogleClientSecret",
+  mongoURI: "myMongoURI",
+  cookieKey: "myCookieKEy",
+};
