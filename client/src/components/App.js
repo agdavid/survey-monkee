22b6af5696241ff9062e77340e7fe08c21621aa4
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2021-06-30 10:26:11 -0400

    route configuration

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,31 @@
 // rendering layer control (React)
 import React from "react";
+// import the router brain (look at URL and handle component display)
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => {
+  return <h2>Header</h2>;
+};
+const Dashboard = () => {
+  return <h2>Dashboard</h2>;
+};
+const SurveyNew = () => {
+  return <h2>SurveyNew</h2>;
+};
+const Landing = () => {
+  return <h2>Landing</h2>;
+};
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Landing} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
