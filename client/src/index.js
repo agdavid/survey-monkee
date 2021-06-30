252e6e087d78e55138274b675b5227e26b02a9ac
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2021-06-30 09:59:13 -0400

    redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,18 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+// redux store at top-level of application
+const store = createStore(() => [], {}, applyMiddleware());
+
+// wrap App in Provider so all children components have access to store
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
