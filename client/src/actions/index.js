50bd888ac8660cce20ae029ecf58aab31fb1b0e7
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2021-07-11 15:40:19 -0400

    refactor fetchUser to async await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,13 +2,7 @@ import axios from "axios";
 
 import { FETCH_USER } from "./types";
 
-export const fetchUser = () => {
-  // request to backend
-  // redux-thunk will see a returned function
-  // and use dispatch function to forward action to all reducers
-  return function (dispatch) {
-    axios
-      .get("/api/current_user")
-      .then((res) => dispatch({ type: FETCH_USER, payload: res }));
-  };
+export const fetchUser = () => async (dispatch) => {
+  const res = await axios.get("/api/current_user");
+  dispatch({ type: FETCH_USER, payload: res });
 };
