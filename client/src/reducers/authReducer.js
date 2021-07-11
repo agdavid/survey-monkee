387605b3472f4ac9a49b5f92be22030bd7462c3c
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2021-07-11 15:55:04 -0400

    authReducer return values

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,17 @@
+import { FETCH_USER } from "../actions/types";
+
 // create and export reducer
-const authReducer = (state = {}, action) => {
-  console.log(action);
+const authReducer = (state = null, action) => {
   switch (action.type) {
+    case FETCH_USER:
+      // logged out: "" || false
+      // logged in: the user model
+      // {
+      //   googleId: "105678131036301297788"
+      //   __v: 0
+      //   _id: "60bd19785e9e50866f8781eb"
+      // }
+      return action.payload || false;
     default:
       return state;
   }
