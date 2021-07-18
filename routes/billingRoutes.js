5542a268541f8ee83af56b5affe27960208b9455
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2021-07-18 17:23:26 -0400

    require authentication to create charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,6 +3,10 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = (app) => {
   app.post("/api/stripe", async (req, res) => {
+    if (!req.user) {
+      // unauthorized / forbidden
+      return res.status(401).send({ error: "You must log in" });
+    }
     const charge = await stripe.charges.create({
       amount: 500,
       currency: "usd",
