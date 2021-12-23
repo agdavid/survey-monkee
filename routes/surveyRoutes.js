c24b8d2e7a83dc174c69f4ecd872cd8d8cc35cb3
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2021-12-23 16:15:27 -0500

    create new Survey

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,21 @@
+const mongoose = require("mongoose");
 // check if user is logged in
 const requireLogin = require("../middlewares/requireLogin");
 // check if user has credits
 const requireCredits = require("../middlewares/requireCredits");
 
+const Survey = mongoose.model("surveys");
+
 module.exports = (app) => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {});
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    // check properties
+    const { title, subject, body, recipients } = req.body;
+
+    // create new instance of Survey
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+    });
+  });
 };
